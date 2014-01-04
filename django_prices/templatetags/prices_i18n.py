7422186e4b407b15ef80e3d59a065d3f1feac7e3
Paul Becotte <pjbecotte@gmail.com>
2014-01-04 02:27:44 -0500

    Update prices_i18n.py
    By default, babel will look at an environment variable to determine locale for currency.  Since this doesn't look at the user's computer, we need to call this function with the locale variable set.

--- django_prices/templatetags/prices_i18n.py
+++ django_prices/templatetags/prices_i18n.py
@@ -1,19 +1,21 @@
 from babel.numbers import format_currency
 from django import template
+from django.utils import translation
 
 register = template.Library()
-
+language = translation.get_language()
+locale = translation.to_locale(language)
 
 @register.simple_tag
 def gross(price):
-    return format_currency(price.gross, price.currency)
+    return format_currency(price.gross, price.currency, locale=locale)
 
 
 @register.simple_tag
 def net(price):
-    return format_currency(price.net, price.currency)
+    return format_currency(price.gross, price.currency, locale=locale)
 
 
 @register.simple_tag
 def tax(price):
-    return format_currency(price.tax, price.currency)
+    return format_currency(price.gross, price.currency, locale=locale)
