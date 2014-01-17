e9d4db3343f645ef1129faea5c5e398d769e0194
Patryk Zawadzki <patrys@room-303.com>
2014-01-17 14:17:50 +0100

    Add more tests

--- django_prices/tests.py
+++ django_prices/tests.py
@@ -4,7 +4,9 @@ from unittest import TestCase
 from django.db import models
 from prices import Price
 
+from . import forms
 from .models import PriceField
+from . import widgets
 
 
 class Foo(models.Model):
@@ -29,3 +31,22 @@ class PriceFieldTest(TestCase):
         field = PriceField('price', currency='BTC', default='5', max_digits=9,
                            decimal_places=2)
         self.assertEqual(field.to_python(7), Price(7, currency='BTC'))
+
+    def test_to_python_handles_none(self):
+        field = PriceField('price', currency='BTC', default='5', max_digits=9,
+                           decimal_places=2)
+        self.assertEqual(field.to_python(None), None)
+
+    def test_to_python_checks_currency(self):
+        field = PriceField('price', currency='BTC', default='5', max_digits=9,
+                           decimal_places=2)
+        invalid = Price(1, currency='USD')
+        self.assertRaises(ValueError, lambda: field.to_python(invalid))
+
+    def test_formfield(self):
+        field = PriceField('price', currency='BTC', default='5', max_digits=9,
+                           decimal_places=2)
+        form_field = field.formfield()
+        self.assertTrue(isinstance(form_field, forms.PriceField))
+        self.assertEqual(form_field.currency, 'BTC')
+        self.assertTrue(isinstance(form_field.widget, widgets.PriceInput))
