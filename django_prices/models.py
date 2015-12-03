c17034394705f5c173f5756ea9c2d14eea525869
Patryk Zawadzki <patrys@room-303.com>
2015-12-03 10:03:07 +0100

    Don't use builtin name as param

--- django_prices/models.py
+++ django_prices/models.py
@@ -18,7 +18,7 @@ class Creator(object):
     def __init__(self, field):
         self.field = field
 
-    def __get__(self, obj, type=None):
+    def __get__(self, obj, objtype=None):
         if obj is None:
             return self
         return obj.__dict__[self.field.name]
