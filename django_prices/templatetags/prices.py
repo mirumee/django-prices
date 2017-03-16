68a10246edf87bad1eb055bbd6eff4c25041fda2
Artur Smęt <artur.smet@mirumee.com>
2017-03-16 11:53:06 +0100

    Fix standard templatetags

--- django_prices/templatetags/prices.py
+++ django_prices/templatetags/prices.py
@@ -13,19 +13,19 @@ def normalize_price(price, normalize):
 
 @register.inclusion_tag('prices/price.html')
 def gross(price, normalize=False):
-    return {'amount': normalize_price(price, normalize),
+    return {'amount': normalize_price(price.gross, normalize),
             'currency': price.currency}
 
 
 @register.inclusion_tag('prices/price.html')
 def net(price, normalize=False):
-    return {'amount': normalize_price(price, normalize),
+    return {'amount': normalize_price(price.net, normalize),
             'currency': price.currency}
 
 
 @register.inclusion_tag('prices/price.html')
 def tax(price, normalize=False):
-    return {'amount': normalize_price(price, normalize),
+    return {'amount': normalize_price(price.tax, normalize),
             'currency': price.currency}
 
 
