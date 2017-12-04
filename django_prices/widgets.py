895852194b316fe8012d45992a96208d83be12f5
Tung Dao <me@tungdao.com>
2017-12-04 10:45:17 +0700

    Django 2.0 compat fix

--- django_prices/widgets.py
+++ django_prices/widgets.py
@@ -2,7 +2,6 @@ from __future__ import unicode_literals
 
 from django import forms
 from django.template.loader import render_to_string
-
 from prices import Price
 
 __all__ = ['PriceInput']
@@ -16,10 +15,13 @@ class PriceInput(forms.TextInput):
         self.currency = currency
         super(PriceInput, self).__init__(*args, **kwargs)
 
-    def _format_value(self, value):
+    def format_value(self, value):
         if isinstance(value, Price):
             value = value.net
         return value
+
+    def _format_value(self, value):
+        return self.format_value(value)
 
     def render(self, name, value, attrs=None):
         widget = super(PriceInput, self).render(name, value, attrs=attrs)
