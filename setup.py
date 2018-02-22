d59d99eabab94a70b7a3cac136f587d08485c178
Rafał Pitoń <rafal@mirumee.com>
2018-02-22 13:14:31 +0100

    Add missing primary_key flag to TaxedModelField that Django accesses when saving model with relations

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     author_email='hello@mirumee.com',
     description='Django fields for the prices module',
     license='BSD',
-    version='1.0.0-beta5',
+    version='1.0.0-beta6',
     url='https://github.com/mirumee/django-prices',
     packages=['django_prices', 'django_prices.templatetags'],
     include_package_data=True,
