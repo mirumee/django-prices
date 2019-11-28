53e8317f906f52e46af8b1d8f7a1577aff70d632
Pavel Zagrebelin <pavel@zagrebelin.ru>
2019-11-28 15:19:15 +0500

    Added verbose_name field to the TaxedMoneyField model

--- django_prices/models.py
+++ django_prices/models.py
@@ -143,6 +143,7 @@ class TaxedMoneyField(NonDatabaseFieldBase):
         self.net_amount_field = net_amount_field
         self.gross_amount_field = gross_amount_field
         self.currency = currency
+        self.verbose_name = verbose_name
 
     def __str__(self):
         return (
